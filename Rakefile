49f73ff6dd5831ddfdf23ea5d3c728964a702885
Jan Berkel <jan.berkel@gmail.com>
2010-11-16 20:45:58 +0100

    grouped by decade

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ references  = 'references.csv'
 imdb_mirror =  URI.parse('ftp://ftp.fu-berlin.de/pub/misc/movies/database/')
 movie_links =  URI.parse(imdb_mirror.to_s + 'movie-links.list.gz')
 
-CLEAN.include(references)
+CLEAN.include(references, 'graph.dot', 'graph.svg')
 
 def mirror(uri, verbose = false)
   sh "wget #{verbose ? '-v' : '-q'} -c --no-host-directories -N -r -l 1 --no-remove-listing #{uri}"
@@ -23,7 +23,14 @@ file references => movie_links.path do
   sh "zcat < #{movie_links.path} | ./links.rb references"
 end
 
-desc "calculate rankings"
 task :rank => references do
-  sh "./rank.py #{references}"
+  sh "./rank.py #{references} --rank"
+end
+
+file 'graph.dot' => references do
+  sh "./rank.py #{references} --graph --max=#{ENV['MAX'] || 150} > graph.dot"
+end
+
+file 'graph.svg' => 'graph.dot' do
+ sh "dot -Tsvg graph.dot -o graph.svg"
 end
--- rank.py
+++ rank.py
@@ -3,6 +3,7 @@
 import networkx as nx
 import csv
 import sys
+import re
 from pprint import pprint as pp
 
 def unicode_csv_reader(unicode_csv_data, **kwargs):
@@ -20,26 +21,64 @@ def read_graph(f):
       g.add_edge(row[0], connection)
   return g
 
+def write_graph(g, out = sys.stdout):
+  out.write("strict digraph {\n")
+  out.write('rankdir=LR; ranksep=.75; size="10,20"; ratio=auto;\n')
+
+  for (y, nodes) in group_nodes(g.nodes()).items():
+    out.write("{ rank = same;")
+    for n in (n for n in nodes if g.degree(n) > 0):
+      out.write(q(n)+';')
+    out.write("}\n")
+
+  for (t,s) in g.edges():
+    out.write('%s -> %s;\n' % (q(s), q(t)))
+
+  out.write("}\n")
+
+
+def group_nodes(nodes):
+  grouped = {}
+  for n in nodes:
+    match = re.search(r'\((\d+)\)$', n)
+    if match:
+      year = int(match.group(1))
+      decade = year - (year % 10)
+      if not decade in grouped:
+        grouped[decade] = []
+
+      grouped[decade].append(n)
+  return grouped
+
 def sub_graph(g, n, algorithm = nx.pagerank):
   return g.subgraph([v[0] for v in top(algorithm(g), n)])
 
-def quote(s):
-  return s.replace("'", "\\'")
+def q(s):
+  return '"%s"' %  s.replace('"', '\\"')
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
-    print __file__, "<file>"
+  if len(sys.argv) < 3:
+    print __file__, "<file> [--graph|--rank|--hits|--degree] [--max=<n>]"
     sys.exit(1)
 
+  n = 200
+  for arg in sys.argv:
+    m = re.match(r'--max=(\d+)', arg)
+    if m: n = int(m.group(1))
+
   g = read_graph(sys.argv[1])
-  # degree
-  #pp(top(nx.degree(g), 5))
+  if '--graph' in sys.argv:
+    sub = sub_graph(g, n)
+    write_graph(sub)
+  elif '--rank' in sys.argv:
+    pp(top(nx.pagerank(g)))
+  elif '--hits' in sys.argv:
+    # HITS
+    (hubs, authorities) = nx.hits(g)
+    pp(top(hubs, n))
+    pp(top(authorities, n))
+  elif '--degree' in sys.argv:
+    # degree
+    pp(top(nx.degree(g), n))
 
-  #nx.write_dot(g, 'g.dot')
 
-  # pagerank
-  pp(top(nx.pagerank(g)))
-  # HITS
-  #(hubs, authorities) = nx.hits(g)
-  #pp(top(hubs, 10))
-  #pp(top(authorities, 10))
