dca6474568bdbdfd9ab7fa030dbb40d7c844f9f4
Jan Berkel <jan.berkel@gmail.com>
2010-11-16 01:27:24 +0100

    subgraph

--- rank.py
+++ rank.py
@@ -5,23 +5,41 @@ import csv
 import sys
 from pprint import pprint as pp
 
-file = sys.argv[1]
-
-G = nx.DiGraph()
-for row in csv.reader(open(file, 'rb'), delimiter='|'):
-  for connection in row[1:]:
-    G.add_edge(row[0], connection)
+def unicode_csv_reader(unicode_csv_data, **kwargs):
+    csv_reader = csv.reader(unicode_csv_data, **kwargs)
+    for row in csv_reader:
+      yield [unicode(cell, 'utf-8').encode('utf-8') for cell in row]
 
 def top(nodes, n = 250):
   return sorted(nodes.items(), key=lambda(k,v): (v,k), reverse=True)[0:n]
 
-# degree
-pp(top(nx.degree(G), 5))
+def read_graph(f):
+  g = nx.DiGraph()
+  for row in unicode_csv_reader(open(f, 'r'), delimiter='|'):
+    for connection in row[1:]:
+      g.add_edge(row[0], connection)
+  return g
 
-# pagerank
-pp(top(nx.pagerank(G)))
+def sub_graph(g, n, algorithm = nx.pagerank):
+  return g.subgraph([v[0] for v in top(algorithm(g), n)])
 
-# HITS
-(hubs, authorities) = nx.hits(G)
-pp(top(hubs, 10))
-pp(top(authorities, 10))
+def quote(s):
+  return s.replace("'", "\\'")
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print __file__, "<file>"
+    sys.exit(1)
+
+  g = read_graph(sys.argv[1])
+  # degree
+  #pp(top(nx.degree(g), 5))
+
+  #nx.write_dot(g, 'g.dot')
+
+  # pagerank
+  pp(top(nx.pagerank(g)))
+  # HITS
+  #(hubs, authorities) = nx.hits(g)
+  #pp(top(hubs, 10))
+  #pp(top(authorities, 10))
